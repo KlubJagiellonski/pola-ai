e18bdd8d0ce8d530847496c297fb8f50408bd4a0
Jakub Lipinski <jakub.lipinski@gmail.com>
2018-11-30 14:15:15 +0100

    upgraded to Python 3 async download of ai pic files

--- data/get_ai_pics.py
+++ data/get_ai_pics.py
@@ -3,10 +3,13 @@ import requests
 import argparse
 import operator
 import os
-from urlparse import urlparse
+import asyncio
+from aiohttp import ClientSession
+from urllib.parse import urlparse
 from PIL import Image
 
-POLISH_LETTERS = [u'ąćęłóźżĄĆĘŁÓŹŻ',u'acelozzACELOZZ']
+MAX_NO_REQUESTS = 50
+POLISH_LETTERS = [u'ąćęłńóźżĄĆĘŁÓŹŻ',u'acelnozzACELOZZ']
 
 def normalize_name(name):
     name = name[:30].strip()
@@ -14,6 +17,50 @@ def normalize_name(name):
         name = name.replace(POLISH_LETTERS[0][i], POLISH_LETTERS[1][i])
     name = ''.join([c if ord(c) < 128 and ord(c) > 47 else " " for c in name])
     return name
+
+
+async def download_file(session, url, data_dir, filename, filename_jpg):
+    print('Downloading: '+filename)
+    async with session.get(url) as response:
+        with open(os.path.join(data_dir, filename), 'wb') as f:
+            content = await response.read()
+            f.write(content)
+
+        if filename.endswith('.png'):
+            im = Image.open(os.path.join(data_dir, filename))
+            if im.mode != 'RGB':
+                im = im.convert('RGB')
+            im.save(os.path.join(data_dir, filename_jpg), quality=100)
+            os.remove(os.path.join(data_dir, filename))
+
+async def bound_download_file(sem, session, url, data_dir, filename, filename_jpg):
+    # Getter function with semaphore.
+    async with sem:
+        return await download_file(session, url, data_dir, filename, filename_jpg)
+
+async def download_files(aipics):
+    tasks = []
+    sem = asyncio.Semaphore(MAX_NO_REQUESTS)  # create instance of Semaphore; limit open requests to MAX_NO_REQUESTS
+    async with ClientSession() as session:
+        for aipic in aipics:
+            if aipic['code'] in top_codes:
+
+                prod = u'{}#{}'.format(normalize_name(aipic['product_name']), aipic['company_id'])
+                dir = os.path.join(data_dir, prod)
+                if not os.path.exists(dir):
+                    os.makedirs(dir)
+                existing_dirs.discard(prod)
+
+                file = aipic['url'].split('/')[-1]
+                filename = os.path.join(prod, file)
+                filename_jpg = os.path.splitext(filename)[0] + '.jpg'
+                if not filename_jpg in existing_files:
+                    task = asyncio.ensure_future(
+                        bound_download_file(sem, session, aipic['url'], data_dir, filename, filename_jpg))
+                    tasks.append(task)
+
+                existing_files.discard(filename_jpg)
+        await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("get_ai_pics")
@@ -43,15 +90,20 @@ if __name__ == "__main__":
 
     top_codes = set()
     i = 0
-    while sorted_no_pics[i][1] >= 20:
-        print sorted_no_pics[i][1],':',code_to_name[sorted_no_pics[i][0]]
+    total_no_of_pics = 0
+    while sorted_no_pics[i][1] >= 30:
+        print(sorted_no_pics[i][1],':',code_to_name[sorted_no_pics[i][0]])
         top_codes.add(sorted_no_pics[i][0])
+        total_no_of_pics += sorted_no_pics[i][1]
         i += 1
 
+    print('total pics: ', total_no_of_pics)
     data_dir = args.data_dir
 
     existing_dirs = set()
     existing_files = set()
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
     for dir in os.listdir(data_dir):
         if dir.startswith('.'):
             continue
@@ -61,41 +113,16 @@ if __name__ == "__main__":
                 s = os.path.join(dir, file)
                 existing_files.add(s)
 
-    for aipic in aipics:
-        if aipic['code'] in top_codes:
-
-            prod = u'{}#{}'.format(normalize_name(aipic['product_name']), aipic['company_id'])
-            dir = os.path.join(data_dir, prod)
-            if not os.path.exists(dir):
-                os.makedirs(dir)
-            existing_dirs.discard(prod)
-
-            file = aipic['url'].split('/')[-1]
-            filename = os.path.join(prod, file)
-            filename_jpg = os.path.splitext(filename)[0] + '.jpg'
-            if not filename_jpg in existing_files:
-                print 'Downloading: '+filename
-                r = requests.get(aipic['url'], stream=True)
-                if r.status_code == 200:
-                    with open(os.path.join(data_dir, filename), 'wb') as f:
-                        for chunk in r:
-                            f.write(chunk)
-
-                    if filename.endswith('.png'):
-                        im = Image.open(os.path.join(data_dir, filename))
-                        if im.mode != 'RGB':
-                            im = im.convert('RGB')
-                        im.save(os.path.join(data_dir, filename_jpg), quality=100)
-                        os.remove(os.path.join(data_dir, filename))
-
-            existing_files.discard(filename_jpg)
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(download_files(aipics))
+    loop.run_until_complete(future)
 
     for filename in existing_files:
-        print 'Deleting: '+os.path.join(data_dir, filename)
+        print('Deleting: '+os.path.join(data_dir, filename))
         os.remove(os.path.join(data_dir, filename))
 
     for dir in existing_dirs:
-        print 'Deleting: '+os.path.join(data_dir, dir)
+        print('Deleting: '+os.path.join(data_dir, dir))
         ds_store = os.path.join(data_dir, dir, '.DS_Store')
         if os.path.exists(ds_store):
             os.remove(ds_store)
