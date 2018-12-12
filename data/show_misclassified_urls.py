7199a1870cc26f4c3bf87fc655db82f12c236c8e
Jakub Lipinski <jakub.lipinski@gmail.com>
2018-12-12 14:53:23 +0100

    convert misclassified images into html file

--- data/show_misclassified_urls.py
+++ data/show_misclassified_urls.py
@@ -4,21 +4,30 @@ if __name__ == "__main__":
     aipics = dict()
     with open('misclassified_images.txt') as f:
         for line in f.readlines():
-            m = re.search(r'.*#(\d*)\/(\d*)_(\d*)[\w\-]*.jpg\s*(.*)',line)
+            line = line.strip()
+            m = re.search(r'.*#(\d*)\/(\d*)_(\d*)[\w\-]*.jpg\s*(.*) (\d*)',line)
             company_id = m.group(1)
             aipic_id = m.group(2)
             idx = m.group(3)
             label = m.group(4)
+            wrong_company_id = m.group(5)
+
+            if company_id == wrong_company_id:
+                continue
 
             aipic = aipics.get(aipic_id, {'indexes':set(), 'labels':set()})
             aipic['indexes'].add(idx)
             aipic['labels'].add(label)
             aipics[aipic_id] = aipic
     
-    for aipic_id, aipic in aipics.items():
-        print(','.join(aipic['indexes']))
-        print(','.join(aipic['labels']))
-        print('https://www.pola-app.pl/cms/ai_pics/{}'.format(aipic_id))
-        print()
+    with open("misclassified_images.html", "w") as f:
+        for aipic_id, aipic in aipics.items():
+            f.write(
+                '<br>'\
+                '<div><strong>{}</strong>{}</div>'\
+                '<iframe width="100%" height="500" src="https://www.pola-app.pl/cms/ai_pics/{}"></iframe>'.format(
+                    ','.join(aipic['labels']), ','.join(aipic['indexes']), aipic_id
+                )
+            )
     
     print(len(aipics))
