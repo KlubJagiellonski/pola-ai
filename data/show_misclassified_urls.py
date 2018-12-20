addb89a530abf1b41444fe1a61566d98a6c92d30
Jakub Lipinski <jakub.lipinski@gmail.com>
2018-12-20 21:51:04 +0100

    show how many training images is available for wrongly classified image

--- data/show_misclassified_urls.py
+++ data/show_misclassified_urls.py
@@ -1,6 +1,17 @@
+import os
 import re
 
+photo_dir = 'Pola_ai'
+
 if __name__ == "__main__":
+    company_id2count = {}
+    for subdir in os.listdir(photo_dir):
+        if subdir.startswith('.'):
+            continue
+        m = re.search(r'.*#(\d*)', subdir)
+        company_id = m[1]
+        company_id2count[company_id] = len([name for name in os.listdir(os.path.join(photo_dir, subdir)) if name.endswith('.jpg')])
+
     aipics = dict()
     with open('misclassified_images.txt') as f:
         for line in f.readlines():
@@ -18,15 +29,16 @@ if __name__ == "__main__":
             aipic = aipics.get(aipic_id, {'indexes':set(), 'labels':set()})
             aipic['indexes'].add(idx)
             aipic['labels'].add(label)
+            aipic['count']=company_id2count[wrong_company_id]
             aipics[aipic_id] = aipic
     
     with open("misclassified_images.html", "w") as f:
         for aipic_id, aipic in aipics.items():
             f.write(
                 '<br>'\
-                '<div><strong>{}</strong>{}</div>'\
+                '<div><strong>{}</strong> {} ({})</div>'\
                 '<iframe width="100%" height="500" src="https://www.pola-app.pl/cms/ai_pics/{}"></iframe>'.format(
-                    ','.join(aipic['labels']), ','.join(aipic['indexes']), aipic_id
+                    ','.join(aipic['labels']), ','.join(aipic['indexes']), aipic['count'], aipic_id
                 )
             )
     
